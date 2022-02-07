97288d979ab9e4b9b98d9f46477629091726467c
Yana Flaryanovich <Yana_Flaryanovich@epam.com>
2022-02-07 23:06:55 +0300

    Add triggers

---
+++ Triggers/t_InsertEmployee.sql
@@ -0,0 +1,6 @@
+﻿CREATE TRIGGER t_InsertEmployee
+     ON dbo.[Employee]
+     AFTER INSERT
+     AS
+     INSERT INTO dbo.[Company] (Name, AddressId)
+     SELECT CompanyName, AddressId FROM INSERTED
